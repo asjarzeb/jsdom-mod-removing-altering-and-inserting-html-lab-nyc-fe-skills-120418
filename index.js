0c33cb96cc1c68cfe8d0638cb250b64eee0a5a0e
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-19 23:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ document.body.remove(main)
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 
+let myName = "AJ"
+
+let newHeader = document.createElement("h1"); newHeader.innertext = victory (myName)
