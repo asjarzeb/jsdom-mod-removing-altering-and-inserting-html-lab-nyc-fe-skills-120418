1b5b798e1acb2ee80c82a843e8bc996f6a3e8e1e
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-19 23:35:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 
-document.body.appendChild(element)
-
-var ul = document.createElement('ul')
-
-element.style.textAlign = 'center';
-ul.style.textAlign = 'left'
-
-ul.removeChild(ul.querySelector('li:nth-child(2)'))
+remove(main)
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
