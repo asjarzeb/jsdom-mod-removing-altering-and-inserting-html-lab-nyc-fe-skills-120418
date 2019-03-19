d605cd302357d1c60aedd98a76a6a8b62788c00a
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-19 23:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 
-'main': expected <main id="main"></main> to not exist
+document.body.remove(main)
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
